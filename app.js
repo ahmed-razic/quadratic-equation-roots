d6549a92d47a4ed8b1b69a4312a38c292a0e1eef
ahmed-razic <ahmed.razic@gmail.com>
2022-04-15 00:17:06 +0200

    navbar & footer

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instance = M.Sidenav.init(elems, {});
+  instance.open();
+});
